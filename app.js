4913d39bbea5d51eb0a5839caae405cb555b3a94
Evgeny Shumilov <ttrrooll700@yandex.ru>
2021-07-05 15:42:29 +0700

    fix: исправил доступ

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(requestLogger);
 app.use("/", require("./routes/index"));
 
 app.use((req, res, next) => {
-  res.header({ "Access-Control-Allow-Origin": "*" });
+  res.header({ "Access-Control-Allow-Origin": "https://movies-f.students.nomoredomains.club" });
   next();
 });
 
