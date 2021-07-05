a7a1c0f422fab127e21109d9ec61c2ca93a2de30
Evgeny Shumilov <ttrrooll700@yandex.ru>
2021-07-05 16:59:22 +0700

    fix: исправил

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,4 @@
 const router = require("express").Router();
-const cors = require("cors");
-
-router.use(cors());
 
 router.use("/", require("./users"));
 router.use("/movies", require("./movies"));
