6dc260fdb7b0178ccdfafbfa6d25fb198593c46a
Evgeny Shumilov <ttrrooll700@yandex.ru>
2021-07-07 23:01:34 +0700

    fix: cors

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const { celebrate, Joi, errors } = require("celebrate");
+const validator = require("validator");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+const auth = require("./middlewares/auth");
 const {
-  login, createUser,
+  updateProfile, getUser, login, createUser,
 } = require("./controllers/users");
+const { getMovies, createMovie, deleteMovie } = require("./controllers/movies");
 
 const { NODE_ENV, MONGOOSE_LINK } = process.env;
 const NotFoundError = require(("./errors/not-found-err"));
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3005 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 mongoose.connect(NODE_ENV === "production" ? MONGOOSE_LINK : "mongodb://localhost:27017/moviesdb", {
   useNewUrlParser: true,
@@ -27,6 +29,8 @@ mongoose.connect(NODE_ENV === "production" ? MONGOOSE_LINK : "mongodb://localhos
 });
 
 app.use(requestLogger);
+
+app.use(cors());
 
 app.post("/signin", celebrate({
   body: Joi.object().keys({
@@ -43,7 +47,49 @@ app.post("/signup", celebrate({
   }),
 }), createUser);
 
-app.use("/", require("./routes/index"));
+app.get("/users/me", auth, getUser);
+
+app.patch("/users/me", auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+  }),
+}), updateProfile);
+
+app.get("/movies", auth, getMovies);
+
+app.post("/movies", auth, celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.number().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message("Не валидная ссылка");
+    }),
+    trailer: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message("Не валидная ссылка");
+    }),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    thumbnail: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message("Не валидная ссылка");
+    }),
+    movieId: Joi.number().required(),
+  }),
+}), createMovie);
+
+app.delete("/movies/:id", auth, deleteMovie);
 
 app.use((req, res, next) => {
   const error = new NotFoundError("Ресурс не найден");
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
 const { celebrate, Joi } = require("celebrate");
-
 const {
-  updateProfile, getUser,
+  updateProfile, getUser, login, createUser,
 } = require("../controllers/users");
+
 const auth = require("../middlewares/auth");
+
+router.post("/signin", celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.post("/signup", celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 router.get("/users/me", auth, getUser);
 
